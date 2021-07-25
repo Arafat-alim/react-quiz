f47ee04dc35ba916995473f1190db1297bfc1750
Arafat <arafat97alim@gmail.com>
2021-07-26 01:02:47 +0530

    storing state inside the component.

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,13 +1,42 @@
 import React from "react";
-
+import { useReducer } from "react";
 import Questions from "./Questions";
 
+const initialState = {
+  // its an simple object
+  currentQuestionIndex: 0,
+  questions: [],
+};
+
+const reducer = (state, action) => {
+  //its a function
+  if (action.type === "NEXT_QUESTION") {
+    return {
+      //return an object
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+  console.log("reducer", state, action);
+  return state;
+};
 const Quiz = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log("state", state);
+  // const [question, setQuestion] = useState([]);
+  // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // default value is zero
+  // console.log("currentQuestionIndex", currentQuestionIndex);
   return (
     <div className="quiz">
       <div className="score">Question 1/10</div>
       <Questions />
-      <div className="next-button">Next Question</div>
+      <div
+        className="next-button"
+        // onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
+        onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+      >
+        Next Question
+      </div>
     </div>
   );
 };
