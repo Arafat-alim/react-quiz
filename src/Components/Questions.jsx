b0ea98d771e695091b522985cd9dcb5f99ef4fae
Arafat <arafat97alim@gmail.com>
2021-07-25 19:44:08 +0530

    Portion move from quiz comp to their resp component.

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Answers from "./Answers";
 
 const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <div>
+      <div>Questions</div>
+      <Answers />
+      <Answers />
+      <Answers />
+      <Answers />
+    </div>
+  );
 };
 
 export default Questions;
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Answers from "./Answers";
+
 import Questions from "./Questions";
 
 const Quiz = () => {
@@ -7,10 +7,6 @@ const Quiz = () => {
     <div>
       <div>Quiz</div>
       <Questions />
-      <Answers />
-      <Answers />
-      <Answers />
-      <Answers />
     </div>
   );
 };
